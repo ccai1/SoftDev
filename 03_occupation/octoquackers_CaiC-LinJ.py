c270477bb7507b8d6065e0c975d7f9bcb081202f
ccai1 <ccai1@stuy.edu>
2018-09-13 22:21:27 -0400

    updated 03_occupation:

--- 03_occupation/octoquackers_CaiC-LinJ.py
+++ 03_occupation/octoquackers_CaiC-LinJ.py
@@ -1,6 +1,6 @@
-#Team Octoquackers - Cathy Cai and Jason Lin
+#Team OctoQuackers -- Cathy Cai and Jason Lin
 #SoftDev1 pd07
-#K3 -- StI/O: Divine your Destiny!
+#K06 -- StI/O: Divine your Destiny!
 #2018-09-13
 
 import csv
@@ -8,42 +8,44 @@ from random import randint
 
 def makeDict(file, dictionary):
 
-    ##open csv file
+    # Open csv file
     with open(file, newline='') as csvfile:
 
-        ##a reader object iterates through
-        ##and separates each row by comma into a string array
+        # A reader object iterates through
+        # and separates each row by comma into a string array
         csvfile = csv.reader(csvfile, delimiter=',')
 
-        ##for each row, assign string 0 as the key, and
-        ##string 1 in float form as the value
-        ##exclude the first and the "Total" row
+        # For each row, assign string 0 as the key, and
+        # string 1 in int (percentage*10) form as the value
+        # exclude the first and the "Total" row
         rowNum = 0
         for row in csvfile:
             if rowNum != 0:
-                dictionary[row[0]] = float(row[1])
+                dictionary[row[0]] = int(float(row[1])*10)
             rowNum += 1
         dictionary.pop('Total')
 
-occupations = {} 
-makeDict('occupations.csv', occupations)
-
 def randOccupation():
 
-    ##num is a random float from 0 - 99.8 with 1 decimal point
-    num = randint(0, 998) / 10.
+    occupations = {} 
+    makeDict('occupations.csv', occupations)
 
-    ##sum the percentages of the occupations until
-    ##you get to a sum greater than num
-    ##(the interval between sums is your percentage)
-    ##break once this is true
+    # num is a random int from 0 - 998
+    num = randint(0, 998)
+
+    # Sum the percentages of the occupations until
+    # you get to a sum greater than num
+    # (the interval between sums is your percentage)
+    # Break once this is true
     Sum = 0
     for occupation in occupations:
-        Sum += occupations[occupation]
+        Sum += occupations[occupation] 
         if Sum >= num:
-            print (occupation)
+            return occupation
             break
-        
-randOccupation()
+
+print (randOccupation())
+
+
 
     
