bf0debb1aa42db9ee9749c03c41edcd0c46cc457
ccai1 <ccai1@stuy.edu>
2018-09-24 23:23:21 -0400

    updated hw10

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,30 +1,38 @@
 #Cathy Cai and Thomas Lee - CaiLee
 #SoftDev1 pd6
-#K10 -- Jinja Tuning
+#K 10 -- Jinja Tuning
 #2018-09-23
 
 from flask import Flask, render_template
-from octoquackers import makeDict, randOccupation, occupations
+from octoquackers import makeDict, randOccupation, occupations #allows us to import our occupations functions and csv file with the percentages
 app = Flask(__name__)
 
 @app.route('/')
-    
+
+#Generates a homepage with a link to our occupations page
 def home():
     return '''<h3>Welcome to your future.</h3>
-            <a href='/occupations'>Get started</a>
+            <a href='/occupations'>Get started</a> 
             '''
 
+#makes a dictionary that holds all the occupations with its respective links
 linkDict = {}
-makeDict('occupations.csv', linkDict, 0, 2)
+makeDict('occupations.csv', linkDict, 0, 2) 
 
 @app.route('/occupations')
 
 def nothome():
+    #Generates a random occupation based on the given percentage 
     rOccupation = randOccupation()
+
+    #accesses the html file within the 'templates' folder
     return render_template('/occupations.html',
-                           collection=occupations,
+
+                           #assign variables that are used in the html file
+                           collection=occupations, 
+                           links=linkDict,
                            randomOccupation=rOccupation,
-                           occupationLink=linkDict[rOccupation]
+                           occupationLink=linkDict[rOccupation] #link associated with that random occupation
                            )
 
 
