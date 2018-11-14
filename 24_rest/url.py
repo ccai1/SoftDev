9e32da2985062e822273493f80a390138db33ffc
ccai1 <ccai1@stuy.edu>
2018-11-13 21:28:56 -0500

    added hw

---
+++ 24_rest/url.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import urllib.request
+import json
+import ssl
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+
+    # Python 3.6 on OSX has no certificates at all, and can't validate any SSL connections
+    context = ssl._create_unverified_context()
+
+    # accessing and reading api
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=qW3bb6KgHGBkm5oDsidiolh2VvzenUI4zFpIjObw", context=context)
+    info = u.read()
+    print (info)
+
+    #converting the json object string into a dict
+    info = json.loads(info)
+
+    #passing in "url" value to template
+    return render_template("index.html",
+                           pic=info["url"]
+                           )
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
