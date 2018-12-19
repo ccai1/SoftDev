a19be096295f4df05693c2d5b520fb23192d2b2e
ccai1 <ccai1@stuy.edu>
2018-12-19 04:54:20 -0500

    hw

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -1,20 +1,49 @@
-// Indigo Ink
-// Cathy Cai, Sophia Xia
-// SoftDev1 pd6
-// K28 -- Sequential Progression
-// 2018-12-28
+//Indigo Ink
+//Cathy Cai, Sophia Xia
+//SoftDev1 pd6
+//K28 -- Sequential Progression
+//2018-12-28
+
+var fibb = function(n){
+
+    // base case 0
+    if(n == 0){
+	     return 0;
+    }
+
+    // base case 1
+    else if (n == 1){
+	     return 1;
+    }
+
+    // add prev two
+    else{
+	     return fibb(n -1) + fibb(n -2);
+    }
+};
 
 var gcd = function (a,b) {
 
+    // this means the previous modulo was 0, so a divided into b
+    if (b == 0) {
+	     return a
+    }
 
-  // this means the previous modulo (a % b) was 0, so b divided into a
-  if (b == 0) {
-    return a
-  }
+    // finds the smallest interval between the two numbers,
+    // if that interval fully divides into b, that is the gcd
+    else {
+	     return gcd(b, a % b)
+    }
 
-  // finds the smallest interval between the two numbers,
-  // passes in the smaller of a and b with the interval
-  else {
-    return gcd(b, a % b)
-  }
+}
+
+var stulist = ["james", "tyler", "lauren", "becky", "oliver", "frank"];
+
+var randstu = function(){
+
+    // random num in range
+    var num = Math.floor(Math.random() * stulist.length);
+
+    // index that
+    return stulist[num];
 }
